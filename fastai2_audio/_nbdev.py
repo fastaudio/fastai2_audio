defe106f657d2455a66ac7ee60fe415f0f413047
scart <lucas.scart@geocontrol.com.br>
2019-12-17 16:20:45 -0300

    build lib

--- fastai2_audio/_nbdev.py
+++ fastai2_audio/_nbdev.py
@@ -2,9 +2,43 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"audio_extensions": "00_core.ipynb",
+         "get_audio_files": "00_core.ipynb",
+         "AudioGetter": "00_core.ipynb",
+         "URLs.SPEAKERS10": "00_core.ipynb",
+         "URLs.SPEAKERS250": "00_core.ipynb",
+         "URLs.ESC50": "00_core.ipynb",
+         "AudioItem": "00_core.ipynb",
+         "show_audio_signal": "00_core.ipynb",
+         "OpenAudio": "00_core.ipynb",
+         "get_usable_kwargs": "00_core.ipynb",
+         "AudioSpectrogram": "00_core.ipynb",
+         "show_spectrogram": "00_core.ipynb",
+         "AudioToSpec": "00_core.ipynb",
+         "AudioToMFCC": "00_core.ipynb",
+         "config_from_func": "00_core.ipynb",
+         "AudioConfig": "00_core.ipynb",
+         "RemoveSilence": "01_augment.ipynb",
+         "Resample": "01_augment.ipynb",
+         "CropSignal": "01_augment.ipynb",
+         "SignalShifter": "01_augment.ipynb",
+         "AddNoise": "01_augment.ipynb",
+         "AudioItem.apply_gain": "01_augment.ipynb",
+         "ChangeVolume": "01_augment.ipynb",
+         "AudioItem.cutout": "01_augment.ipynb",
+         "SignalCutout": "01_augment.ipynb",
+         "AudioItem.lose_signal": "01_augment.ipynb",
+         "SignalLoss": "01_augment.ipynb",
+         "DownmixMono": "01_augment.ipynb",
+         "CropTime": "01_augment.ipynb",
+         "MaskFreq": "01_augment.ipynb",
+         "MaskTime": "01_augment.ipynb",
+         "SGRoll": "01_augment.ipynb",
+         "Delta": "01_augment.ipynb",
+         "TfmResize": "01_augment.ipynb"}
 
-modules = []
+modules = ["core.py",
+           "augment.py"]
 
 git_url = "https://github.com/rbracco/fastai2_audio/tree/master/"
 
